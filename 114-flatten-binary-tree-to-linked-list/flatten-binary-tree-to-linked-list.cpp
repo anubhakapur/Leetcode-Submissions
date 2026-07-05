2504f521a7e0a77289793d8a592d0260ba365cce
Anubha Kapur <114015048+anubhakapur@users.noreply.github.com>
2026-07-05 19:59:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (52.27%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -11,21 +11,13 @@
  */
 class Solution {
 public:
-    void findPreorder(TreeNode*root,vector<TreeNode*>&preorder){
-        if(!root)return;
-        preorder.push_back(root);
-        findPreorder(root->left,preorder);
-        findPreorder(root->right,preorder);
-    }
+    TreeNode*nextRight=nullptr;
     void flatten(TreeNode* root) {
-        if(!root)return;
-        vector<TreeNode*>preorder;
-        findPreorder(root,preorder);
-        TreeNode*temp=root;
-        for(int i=1;i<preorder.size();i++){
-            temp->right=preorder[i];
-            temp->left=nullptr;
-            temp=temp->right;
-        }
+       if(!root)return;
+       flatten(root->right);
+       flatten(root->left);
+       root->left=nullptr;
+       root->right=nextRight;
+       nextRight=root;
     }
 };
