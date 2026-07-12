11f8a50aae7ff195df569bdb8c30d36355d2377a
Anubha Kapur <114015048+anubhakapur@users.noreply.github.com>
2026-07-12 19:02:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (19.31%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
-    void rotateArray(vector<int>& nums){
-        int firstElement=nums[0];
-        for(int i=0;i<nums.size()-1;i++){
-            nums[i]=nums[i+1];
-        }
-        nums[nums.size()-1]=firstElement;
-    }
     bool check(vector<int>& nums) {
-        vector<int>sortedArray(nums.begin(),nums.end());
-        sort(sortedArray.begin(),sortedArray.end());
-        int rotations=nums.size();
-        for(int i=1;i<=rotations;i++){
-            rotateArray(nums);
-            if(nums==sortedArray)return true;
+        int n=nums.size();
+        int noOfInversionPairs=0;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]>nums[i+1])noOfInversionPairs++;
         }
-        return false;
+        if(nums[0]<nums[n-1])noOfInversionPairs++;
+        return (noOfInversionPairs<=1);
     }
 };
