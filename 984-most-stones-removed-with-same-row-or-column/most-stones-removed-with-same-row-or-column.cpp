69a402efce9313314eea894d916b7779f2734f62
Anubha Kapur <114015048+anubhakapur@users.noreply.github.com>
2026-07-22 00:18:06 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (65.25%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,23 +1,49 @@
-class Solution {
-public:
-    void dfs(int node,vector<vector<int>>& stones,vector<bool>&visited){
-        visited[node]=true;
-        for(int i=0;i<stones.size();i++){
-            if(!visited[i] && (stones[node][0]==stones[i][0]||stones[node][1]==stones[i][1])){
-                dfs(i,stones,visited);
-            }
+class DisjointSet{
+    public:
+    vector<int>parent,Size;
+    DisjointSet(int n){
+        parent.resize(n+1);
+        Size.resize(n+1,1);
+        for(int i=0;i<=n;i++)parent[i] = i;
+    }
+    int findParent(int u){
+        if(u==parent[u])return u;
+        return parent[u]=findParent(parent[u]);
+    }
+    void UnionBySize(int u,int v){
+        int pu=findParent(u);
+        int pv=findParent(v);
+        if(pu==pv)return;
+        if(Size[pu]<Size[pv]){
+            parent[pu]=pv;
+            Size[pv]+=Size[pu];
+        }else{
+            parent[pv]=pu;
+            Size[pu]+=Size[pv];
         }
     }
+};
+class Solution {
+    public:
     int removeStones(vector<vector<int>>& stones) {
         int nodes=stones.size();
-        vector<bool>visited(nodes,false);
-        int noOfComponents=0;
-        for(int i=0;i<nodes;i++){
-            if(!visited[i]){
-                dfs(i,stones,visited);
-                noOfComponents++;
-            }
+        int maxRow=0;
+        int maxCol=0;
+        for(auto & edge:stones){
+            maxRow=max(maxRow,edge[0]);
+            maxCol=max(maxCol,edge[1]);
         }
-        return (nodes-noOfComponents);
+        DisjointSet ds(maxRow+maxCol+1);
+        for(auto&stone:stones){
+            int x=stone[0];
+            int y=stone[1];
+            ds.UnionBySize(x,y+maxRow+1);
+        }
+        unordered_set<int>components;
+        for (auto &stone:stones) {
+            int row=stone[0];
+            components.insert(ds.findParent(row));
+        }
+        return nodes-components.size();
     }
 };
