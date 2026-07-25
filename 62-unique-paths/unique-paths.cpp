2a6ede123ec909860daa1539a8ca82effd78567f
Anubha Kapur <114015048+anubhakapur@users.noreply.github.com>
2026-07-25 16:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (30.73%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -7,7 +7,14 @@ public:
         return dp[i][j]=solve(i+1,j,m,n,dp)+solve(i,j+1,m,n,dp);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(0,0,m,n,dp);
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i-1>=0)dp[i][j]=dp[i-1][j];
+                if(j-1>=0)dp[i][j]+=dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
