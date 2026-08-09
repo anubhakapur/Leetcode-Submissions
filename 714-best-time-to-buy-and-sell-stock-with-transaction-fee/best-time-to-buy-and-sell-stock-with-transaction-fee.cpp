811f0cb1804f8ab16d26aff781d749c1a335d6f7
Anubha Kapur <114015048+anubhakapur@users.noreply.github.com>
2026-08-09 20:23:06 +0530

    Time: 16 ms (70.76%) | Memory: 63.6 MB (81.28%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int>& prices, int i,int holdingStock,int fee,vector<vector<int>>&dp){
-        if(i>=prices.size())return 0;
-        if(dp[i][holdingStock]!=-1)return dp[i][holdingStock];
-        if(holdingStock==0){
-            int buy=-prices[i]+solve(prices,i+1,1,fee,dp);
-            int skip=solve(prices,i+1,0,fee,dp);
-            return dp[i][holdingStock]=max(buy,skip);
-        }else{ 
-            int sell=prices[i]+solve(prices,i,0,fee,dp)-fee;
-            int skip=solve(prices,i+1,1,fee,dp);
-            return dp[i][holdingStock]=max(sell,skip);
-        }
-    }
     int maxProfit(vector<int>& prices, int fee) {
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,0)); 
+        vector<int>curr(2,0);
+        vector<int>nextDay(2,0);
          for(int i=n-1;i>=0;i--){
-            int buy=dp[i+1][1]-prices[i];
-            int skipBuying=dp[i+1][0];
-            dp[i][0]=max(buy,skipBuying);
+            int buy=nextDay[1]-prices[i];
+            int skipBuying=nextDay[0];
+            curr[0]=max(buy,skipBuying);
 
-            int sell=dp[i][0]+prices[i]-fee;
-            int skipSelling=dp[i+1][1];
-            dp[i][1]=max(sell,skipSelling);
+            int sell=curr[0]+prices[i]-fee;
+            int skipSelling=nextDay[1];
+            curr[1]=max(sell,skipSelling);
+            nextDay=curr;
         }
-        return dp[0][0];
+        return curr[0];
     }
 };
