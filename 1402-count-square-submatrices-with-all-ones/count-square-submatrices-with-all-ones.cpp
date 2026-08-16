cddc14bfeae9537b792b20d37302d91d0eaec5c4
Anubha Kapur <114015048+anubhakapur@users.noreply.github.com>
2026-08-16 14:52:00 +0530

    Time: 20 ms (9.63%) | Memory: 31.7 MB (5.44%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
+    int solve(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>&dp){
+        if(i>=matrix.size()||j>=matrix[0].size()||matrix[i][j]==0){
+            return 0;
+        }
+        if(dp[i][j]!=-1)return dp[i][j];
+        int right=solve(i,j+1,matrix,dp);
+        int bottom=solve(i+1,j,matrix,dp);
+        int diag=solve(i+1,j+1,matrix,dp);
+        return dp[i][j]=(1+min({right,bottom,diag}));
+    }
     int countSquares(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,0)); //how many squares end at (i,j) ie have (i,j) as right bottom
-        int cnt=0;
-        for(int i=0;i<m;i++){//first row=1
-            if(matrix[0][i]==1){
-                dp[0][i]=1;
-                cnt++;
-            }
-        }
-        for(int i=1;i<n;i++){//first col=1 and skipping dp[0][0] as covered above
-            if(matrix[i][0]==1){
-                dp[i][0]=1;
-                cnt++;
-            }
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][j]==1){
-                    dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
-                    cnt+=dp[i][j];
+        int rows = matrix.size();
+        int cols = matrix[0].size(); 
+        int ans = 0;
+        vector<vector<int>> dp(rows, vector<int>(cols, -1));
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (matrix[i][j] == 1) {
+                    ans += solve(i,j,matrix,dp);
                 }
             }
         }
-        return cnt;        
+        
+        return ans;
     }
 };
