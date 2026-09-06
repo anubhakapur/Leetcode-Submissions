808c9729363713c310acb3ee8414bcc80a77b94d
Anubha Kapur <114015048+anubhakapur@users.noreply.github.com>
2026-09-06 12:40:35 +0530

    Time: 3 ms (28.08%) | Memory: 10.4 MB (14.48%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,10 +3,7 @@ public:
     bool isPalindrome(string s) {
         string temp;
         for(char &c:s){
-            if(c>=65 && c<=90){
-                temp+=(c+32);
-            }else if(c>=97 && c<=122)temp+=c;
-            else if(c>='0' && c<='9')temp+=c;
+            if(isalnum(c))temp+=tolower(c);
         }
         cout<<temp;
         int i=0,j=temp.size()-1;
